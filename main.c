9401513ca73f0c5c648d6adc4ee55187d23c303b
Remi-pcn <remiponcin55@gmail.com>
2024-06-05 21:40:31 +0200

    Advisors working, now need to implement the functiuns

--- main.c
+++ main.c
@@ -14,21 +14,25 @@ typedef struct Customer
     double netsalary;
     double loanpayment;
     double balance;
-}Customer;
+} Customer;
 
 typedef struct Advisor
 {
     int advisorID;
     char username[20];
     char password[20];
-    char RIB[50][23];
-}Advisor;
+    char RIB[50][24];
+} Advisor;
 
-int LogInC(Customer* customers, char* username, char* password) {
+int LogInC(Customer *customers, char *username, char *password)
+{
     int i = 0;
-    for (i = 0; i < nb_lines; i++) {
-        if (strcmp(customers[i].username, username) == 0) {
-            if (strcmp(customers[i].password, password) == 0) {
+    for (i = 0; i < nb_lines; i++)
+    {
+        if (strcmp(customers[i].username, username) == 0)
+        {
+            if (strcmp(customers[i].password, password) == 0)
+            {
                 user_id = i;
                 return 1;
             }
@@ -38,11 +42,15 @@ int LogInC(Customer* customers, char* username, char* password) {
     return 0;
 }
 
-int LogInA(Advisor* advisors, char* username, char* password) {
+int LogInA(Advisor *advisors, char *username, char *password)
+{
     int i = 0;
-    for (i = 0; i < nb_lines; i++) {
-        if (strcmp(advisors[i].username, username) == 0) {
-            if (strcmp(advisors[i].password, password) == 0) {
+    for (i = 0; i < nb_lines; i++)
+    {
+        if (strcmp(advisors[i].username, username) == 0)
+        {
+            if (strcmp(advisors[i].password, password) == 0)
+            {
                 advisor_id = i;
                 return 1;
             }
@@ -52,25 +60,31 @@ int LogInA(Advisor* advisors, char* username, char* password) {
     return 0;
 }
 
-int findID(Customer* customers, char* RIB) {
+int findID(Customer *customers, char *RIB)
+{
     int i = 0;
-    for (i = 0; i < nb_lines; i++) {
-        if (strcmp(customers[i].RIB, RIB) == 0) {
+    for (i = 0; i < nb_lines; i++)
+    {
+        if (strcmp(customers[i].RIB, RIB) == 0)
+        {
             return i;
         }
     }
     return -1;
 }
 
-int LogOut(Customer* customers) {
+int LogOut(Customer *customers)
+{
     return 1;
 }
 
-void SaveFiles(Customer* customers) {
-    FILE* file = NULL;
+void SaveFiles(Customer *customers)
+{
+    FILE *file = NULL;
     file = fopen("Data/Customer.txt", "w");
     int i;
-    for (i = 0; i < nb_lines; i++) {
+    for (i = 0; i < nb_lines; i++)
+    {
         fprintf(file, "%s, %d, %s, %s, %s, %lf, %lf, %lf\n", customers[i].RIB, customers[i].advisorID, customers[i].username, customers[i].password, customers[i].birthdate, customers[i].netsalary, customers[i].loanpayment, customers[i].balance);
     }
     fclose(file);
@@ -78,8 +92,10 @@ void SaveFiles(Customer* customers) {
     return;
 }
 
-int Withdraw(Customer* customers, double amount,int id) {
-    if (customers[id].balance >= amount) {
+int Withdraw(Customer *customers, double amount, int id)
+{
+    if (customers[id].balance >= amount)
+    {
         customers[id].balance -= amount;
         return 1;
     }
@@ -87,37 +103,44 @@ int Withdraw(Customer* customers, double amount,int id) {
     return 0;
 }
 
-void Deposit(Customer* customers, double amount, int id) {
+void Deposit(Customer *customers, double amount, int id)
+{
     customers[id].balance += amount;
     return;
 }
 
-double loanEligibility(Customer* customers) {
+double loanEligibility(Customer *customers)
+{
     double max_loan = customers[user_id].netsalary * 0.33;
     max_loan -= customers[user_id].loanpayment;
     return (max_loan);
 }
 
-void transfer(Customer* customers, char* RIB, double amount) {
-    int other_id=findID(customers, RIB);
-    if (Withdraw(customers, amount, user_id) == 1) {
+void transfer(Customer *customers, char *RIB, double amount)
+{
+    int other_id = findID(customers, RIB);
+    if (Withdraw(customers, amount, user_id) == 1)
+    {
         Deposit(customers, amount, other_id);
         return;
     }
     return;
 }
 
-void updateInfo(Customer* customers) {
-    char again='y';
-    while(again=='y'||again=='Y'){
-    printf("\nwhat do you want to update ?\n");
-    printf("1. Username\n");
-    printf("2. Birthdate\n");
-    printf("3. Password\n");
-    printf("4. Salary\n");
-    int choice;
-    scanf("%d", &choice);
-    switch (choice){
+void updateInfo(Customer *customers)
+{
+    char again = 'y';
+    while (again == 'y' || again == 'Y')
+    {
+        printf("\nwhat do you want to update ?\n");
+        printf("1. Username\n");
+        printf("2. Birthdate\n");
+        printf("3. Password\n");
+        printf("4. Salary\n");
+        int choice;
+        scanf("%d", &choice);
+        switch (choice)
+        {
         case 1:
             char new_username[20];
             printf("\nEnter your new username : \n");
@@ -128,7 +151,7 @@ void updateInfo(Customer* customers) {
             char new_birthdate[10];
             printf("\nEnter your new birthdate (dd/mm/yyyy) : \n");
             scanf("%s", &new_birthdate);
-            strcpy(customers[user_id].birthdate, new_birthdate);   
+            strcpy(customers[user_id].birthdate, new_birthdate);
             break;
         case 3:
             char new_password[20];
@@ -146,13 +169,14 @@ void updateInfo(Customer* customers) {
             printf("Invalid choice\n");
             break;
         }
-    printf("\nDo you want to update again ? (y/n) \n");
-    scanf("%c", &again);
+        printf("\nDo you want to update again ? (y/n) \n");
+        scanf("%c", &again);
     }
     return;
 }
 
-void checkInfo(Customer* customers) {
+void checkInfo(Customer *customers)
+{
     printf("Your username is : %s\n", customers[user_id].username);
     printf("Your RIB is : %s\n", customers[user_id].RIB);
     printf("Your advisor ID is : %d\n", customers[user_id].advisorID);
@@ -163,82 +187,112 @@ void checkInfo(Customer* customers) {
     printf("Your balance is : %.2lf\n", customers[user_id].balance);
 }
 
-Customer* loadC() {
-    FILE* file = NULL;
-    Customer* customers;
+Customer *loadC()
+{
+    FILE *file = NULL;
+    Customer *customers;
     int lines = 0;
     char buff[200];
     file = fopen("Data/Customer.txt", "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Error while opening file\n");
         exit(1);
     }
-    while (fgets(buff, 200, file) != NULL) {
+    while (fgets(buff, 200, file) != NULL)
+    {
         lines++;
     }
     nb_lines = lines;
     rewind(file);
-    customers = (Customer*)malloc(nb_lines *sizeof(Customer));
-    if (customers == NULL) { 
-        printf("Memory not allocated.\n"); 
+    customers = (Customer *)malloc(nb_lines * sizeof(Customer));
+    if (customers == NULL)
+    {
+        printf("Memory not allocated.\n");
         exit(1);
-    } 
+    }
     int i = 0;
-    for (i = 0; i < lines; i++) {
+    for (i = 0; i < lines; i++)
+    {
         fscanf(file, "%23[^,], %d, %20[^,], %20[^,], %10[^,], %lf, %lf, %lf\n", customers[i].RIB, &customers[i].advisorID, customers[i].username, customers[i].password, customers[i].birthdate, &customers[i].netsalary, &customers[i].loanpayment, &customers[i].balance);
     }
-    
+
     fclose(file);
     return customers;
 }
 
-Advisor *loadA() {
-    FILE* file = NULL;
-    Advisor* advisors;
-    advisors = (Advisor*)malloc(0);
+Advisor *loadA()
+{
+    FILE *file = NULL;
+    Advisor *advisors = NULL;
     int lines = 0;
     char buff[200];
+
     file = fopen("Data/Advisor.txt", "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Error while opening file\n");
         exit(1);
     }
-    while (fgets(buff, 200, file) != NULL) {
+
+    while (fgets(buff, sizeof(buff), file) != NULL)
+    {
         lines++;
     }
     rewind(file);
+
+    advisors = (Advisor *)calloc(lines, sizeof(Advisor));
+    if (advisors == NULL)
+    {
+        printf("Memory not allocated.\n");
+        exit(1);
+    }
+
     int past_ID = -1;
     int current_advisor = -1;
     int nb_customer = 0;
-    int i = 0;
-    for (i = 0; i < lines; i++) {
-        int advisorID=0;
-        char username[20];
-        char password[20];
-        char RIB[23];
-        fscanf(file, "%d, %20[^,], %20[^,], %23[^,]\n", &advisorID, username, password, RIB);
-        if (advisorID != past_ID) {
+
+    for (int i = 0; i < lines; i++)
+    {
+        int advisorID = 0;
+        char username[20] = {0};
+        char password[20] = {0};
+        char RIB[24] = {0};
+
+        fscanf(file, "%d, %20[^,], %20[^,], %24[^\n]\n", &advisorID, username, password, RIB);
+
+        if (advisorID != past_ID)
+        {
             nb_customer = 0;
             current_advisor++;
-            advisors = (Advisor*)realloc(advisors, (current_advisor+1) * sizeof(Advisor));
-            if (advisors == NULL) { 
-                printf("Memory not allocated.\n"); 
+            advisors = (Advisor *)realloc(advisors, (current_advisor + 1) * sizeof(Advisor));
+            if (advisors == NULL)
+            {
+                printf("Memory not allocated.\n");
                 exit(1);
-            } 
+            }
+            memset(&advisors[current_advisor], 0, sizeof(Advisor)); // Initialize the newly allocated memory with 0
+            advisors[current_advisor].advisorID = advisorID;
             advisors[current_advisor].advisorID = advisorID;
             strcpy(advisors[current_advisor].username, username);
             strcpy(advisors[current_advisor].password, password);
             strcpy(advisors[current_advisor].RIB[nb_customer], RIB);
             past_ID = advisorID;
         }
-        else {
+        else
+        {
             nb_customer++;
-            strcpy(advisors[current_advisor].RIB[nb_customer], RIB);
+            if (nb_customer < 50)
+            {
+                strcpy(advisors[current_advisor].RIB[nb_customer], RIB);
+            }
         }
     }
+
     fclose(file);
     return advisors;
 }
+
 /*
 int main() {
     Customer *customers = loadC();
@@ -283,7 +337,7 @@ int main() {
                 scanf("%d", &choice);
                 switch (choice) {
                     case 1:
-                        printf("\nEnter the amount you want to deposit :\n"); 
+                        printf("\nEnter the amount you want to deposit :\n");
                         scanf("%lf", &amount);
                         Deposit(customers, amount, user_id);
                         printf("Your new balance is : %.2lf\n", customers[user_id].balance);
@@ -369,7 +423,7 @@ int main() {
                 scanf("%d", &choice);
                 switch (choice) {
                     case 1:
-                        printf("\nEnter the amount you want to deposit :\n"); 
+                        printf("\nEnter the amount you want to deposit :\n");
                         scanf("%lf", &amount);
                         Deposit(customers, amount, user_id);
                         printf("Your new balance is : %.2lf\n", customers[user_id].balance);
@@ -412,13 +466,13 @@ int main() {
         SaveFiles(customers);
         printf("Do you want to reconnect? (y / n)\n");
         scanf("%s", &reconnect);
-    }  
+    }
     return 0;
 }
 */
 
-int main(){
-    printf("fndssfdbjvf");
+int main()
+{
     Customer *customers = loadC();
     Advisor *advisors = loadA();
     printf("Welcome to the bank\n");
@@ -426,12 +480,22 @@ int main(){
     char password[20];
     char reconnect = 'y';
     double amount;
-    printf("%d", sizeof(Advisor)/sizeof(&advisors));
-    for (int i=0;i<3;i++){
-        printf("zouip");
-        printf("%d\n", advisors[i].advisorID);
-        printf("%s\n", advisors[i].RIB);
-        printf("%s\n", advisors[i].username);
-        printf("%s\n", advisors[i].password);
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Advisor ID: %d\n", advisors[i].advisorID);
+        printf("Username: %s\n", advisors[i].username);
+        printf("Password: %s\n", advisors[i].password);
+        printf("RIB:\n");
+        for (int j = 0; j < 50; j++)
+        {
+            // Ensure we only print initialized RIBs (assuming empty ones are all zeroes)
+            if (strlen(advisors[i].RIB[j]) > 0)
+            {
+                printf("At rank %d:\n%s\n", j, advisors[i].RIB[j]);
+            }
+        }
+        printf("\n");
     }
+
+    return 0;
 }
